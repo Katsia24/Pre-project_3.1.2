7c564ae44376de2ff7d249e66590ae5772e51416
Katerina Nizhnikova <katerinanizh@gmail.com>
2024-07-26 16:33:13 +0300

    comments correctedÐ

--- src/main/java/ru/nizhnikova/springforkata/model/User.java
+++ src/main/java/ru/nizhnikova/springforkata/model/User.java
@@ -8,16 +8,12 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
 
-    @Column(name = "name")
     private String firstName;
 
-    @Column(name = "last_name")
     private String lastName;
 
-    @Column(name = "email")
     private String email;
 
     public User() {
